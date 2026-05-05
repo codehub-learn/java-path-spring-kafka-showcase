e1ed2baecfdd633126aef80213c5a4dcc4417672
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2026-05-05 14:14:38 +0300

    Refactor Kafka producer and consumer configurations by replacing `KafkaProperties` usage with custom property mappings for enhanced flexibility.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- fos-intro/src/main/java/gr/codelearn/spring/kafka/intro/config/KafkaConsumerConfig.java
+++ fos-intro/src/main/java/gr/codelearn/spring/kafka/intro/config/KafkaConsumerConfig.java
@@ -1,20 +1,42 @@
 package gr.codelearn.spring.kafka.intro.config;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
-import org.springframework.boot.ssl.SslBundles;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class KafkaConsumerConfig {
+	private final String bootstrapServers;
+	private final String groupId;
+
+	KafkaConsumerConfig(
+			@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+			@Value("${spring.kafka.consumer.group-id}") String groupId) {
+		this.bootstrapServers = bootstrapServers;
+		this.groupId = groupId;
+	}
+
+	private Map<String, Object> consumerProperties() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		return props;
+	}
+
 	@Bean
-	public ConsumerFactory<String, String> consumerFactory(KafkaProperties kafkaProperties,
-	                                                       ObjectProvider<SslBundles> sslBundles) {
-		return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties());
+	public ConsumerFactory<String, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerProperties());
 	}
 
 	@Bean
